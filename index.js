b6bd20a40ec09916559f926e22d724b0cfc60846
Krunal Hemnani <42304335+dkrunal31@users.noreply.github.com>
2023-06-08 08:14:14 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const { json } = require('express');
+const express = require('express'); 
+const app = express();     
+
+//Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// Home page request
+app.get('/', (req, res) => { 
+  res.send("Welcome to the microservice");
+}); 
+
+//app.listen(3000, () => { 
+//  console.log('Server is listening on port 3000'); 
+//}); 
+
+app.listen(PORT, HOST, () => { 
+  console.log('Running on http://${HOST}:${PORT}'); 
+}); 
